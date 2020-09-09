7eef4435e952f56c604d3e1b54134af087554237
pcharul <charuli@iiitd.ac.in>
2020-09-09 14:46:53 +0530

    check

--- demo.m
+++ demo.m
@@ -1,38 +1,39 @@
 clear all
  clc
+ addpath(genpath(pwd));
  warning off;
 rng(1);
 % para.dataset =1;
 %     
 %   error_1 = Run_imputation(para);
-   para.dataset =1;
+   para.dataset =2;
 %     
   error_vbfsi = Run_imputation(para);
-%   para.dataset =3;
+  para.dataset =3;
 %     
-%   error_3 = Run_imputation(para);
-   csvwrite("error_act_vbfsi.csv",error_vbfsi);
-%    csvwrite("error_2.csv",error_2);
+  error_3 = Run_imputation(para);
+   csvwrite("error_act2_vbfsi.csv",error_vbfsi);
+    csvwrite("error_act3_vbfsi.csv",error_3);
 %     csvwrite("error_3.csv",error_3);
 
 
 
 
-
-    para.out_per=0.1;
-  [error_v_1,error_r_1] = Run_outlier_imputation(para);
-%   para.dataset =1;
 % 
-    para.out_per=0.05; 
-  [error_v_2,error_r_2] = Run_outlier_imputation(para);
-%   para.dataset =1;
-%         para.out_per=0.02; 
-%    [error_v_3,error_r_3]  = Run_outlier_imputation(para);
-%   
+%     para.out_per=0.1;
+%   [error_v_1,error_r_1] = Run_outlier_imputation(para);
+% %   para.dataset =1;
+% % 
+%     para.out_per=0.05; 
+%   [error_v_2,error_r_2] = Run_outlier_imputation(para);
+% %   para.dataset =1;
+% %         para.out_per=0.02; 
+% %    [error_v_3,error_r_3]  = Run_outlier_imputation(para);
+% %   
+% %    
+%    %csvwrite(strcat("error_v_1.csv"),error_v_1,1);
+%    csvwrite(strcat("error_r_5_1.csv"),error_r_2);
+%    csvwrite(strcat("error_v_5_1.csv"),error_v_2);
 %    
-   %csvwrite(strcat("error_v_1.csv"),error_v_1,1);
-   csvwrite(strcat("error_r_5_1.csv"),error_r_2);
-   csvwrite(strcat("error_v_5_1.csv"),error_v_2);
-   
-   csvwrite(strcat("error_r_1_1.csv"),error_r_1);
-   csvwrite(strcat("error_v_1_1.csv"),error_v_1);
+%    csvwrite(strcat("error_r_1_1.csv"),error_r_1);
+%    csvwrite(strcat("error_v_1_1.csv"),error_v_1);
